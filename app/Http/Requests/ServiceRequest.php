185fa47ec5cb6aa9a1213ffafa1dbd0bb7bc9596
MURAD-LAPTOP\Md Murad Hosen <muradbd.info@gmail.com>
2022-11-12 19:40:01 +0600

    first commit

---
+++ app/Http/Requests/ServiceRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ServiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = request()->id;
+        return [
+            'name'                           => 'required|unique:services,name,'.$id,
+            'category_id'                    => 'required',
+            'provider_id'                    => 'required',
+            'type'                           => 'required',
+            'price'                          => 'required|integer|min:0',   
+            'duration'                       => 'required',   
+            'service_attachment'             => 'required',   
+            'status'                         => 'required',
+        ];
+    }
+}

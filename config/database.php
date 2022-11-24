49892cfb13427ba91e2aea23b2895bbafcd2fce2
Md.Murad Hosen <56858449+muradbdinfo@users.noreply.github.com>
2022-11-24 20:41:15 +0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
+'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'allservicesapp.mysql.database.azure.com'),
@@ -71,7 +71,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('/ssl/DigiCertGlobalRootCA.crt.pem'),
             ]) : [],
         ],
-
+        
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
